e49f36cf1554f6dd2943999804ba3653c06d053f
Nicolas Petton <nicolas@petton.fr>
2016-12-10 17:31:49 +0100

    * jade-interaction.el: Add a commentary.

--- jade-interaction.el
+++ jade-interaction.el
@@ -18,6 +18,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Minor mode for interacting with a Jade connection in JavaScript buffers.
+
 ;;; Code:
 
 (require 'js2-mode)
